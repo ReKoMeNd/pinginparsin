bb180f0946f121b6e1a3e6a66a8afa0af733b3fc
ReKoMeNd <103186677+ReKoMeNd@users.noreply.github.com>
2022-07-19 12:57:54 +0300

    Add files via upload

---
+++ Parser.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import platform
+import subprocess
+import argparse
+
+#Searching for A DNS records
+def dnsrecord(f1):
+    if f1.find("IN	A")!=-1:
+        return f1
+
+#Adding name of parsed file
+parser = argparse.ArgumentParser()
+parser.add_argument('File',
+                    metavar='file',
+                    type=str,
+                    help='name of parsed file')
+
+args = parser.parse_args()
+
+print(args.File)
+
+f = open(args.File, 'r', encoding="utf-8")
+
+sd = ""
+f1 = open('parsedfile.txt', 'w')
+
+while True:
+
+    line = f.readline()
+    if type(dnsrecord(line)) == str:
+        ad = dnsrecord(line).replace(';', '')
+        ad = ad.replace('*', '')
+        if ad.find('	'):
+            sd = sd + ad.partition('	')[0] + '.mai.ru' + '\r\n'
+        else:
+            sd = sd + ad.partition('		')[0] + '.mai.ru' + '\r\n'
+   
+    if not line:
+        sd = sd.replace('\r', '')
+        f1.write(sd)
+        f.close
+        break
+
+print(sd)
+
+
---
+++ pingpong.py
@@ -0,0 +1,47 @@
+import platform
+import subprocess
+import argparse
+
+def myping(host):
+    parameter = '-n' if platform.system().lower()=='windows' else '-c'
+
+    command = ['ping', parameter, '1', host]
+    response = subprocess.call(command)
+
+    if response == 0:
+        return True
+    else:
+        return False
+
+parser = argparse.ArgumentParser()
+parser.add_argument('File',
+                    metavar='file',
+                    type=str,
+                    help='name of parsed file')
+
+args = parser.parse_args()
+f = open(args.File, 'r', encoding="utf-8")
+
+sucpingfile = open('successfullypinged.txt', 'w')
+unsucpingfile = open('unsuccesfullypinged.txt', 'w')
+sucping = ''
+unsucping = ''
+
+while True:
+    line = f.readline()
+    line = line.replace('\r', '')
+    line = line.replace('\n', '')
+    print(line)
+    
+    if myping(line) == True:
+        sucping = sucping + line + '\r\n'
+    else:
+        unsucping = unsucping + line + '\r\n'
+    
+    if not line:
+        sucpingfile.write(sucping)
+        unsucpingfile.write(unsucping)
+        f.close
+        sucpingfile.close
+        unsucpingfile.close
+        break
